2f7ea2cc9469cd87b4278dc2d9d5e7497274dc0e
Devesh Sharma <deveshsharma372002@gmail.com>
2025-09-02 23:51:42 +0530

    Login and logout api

--- NodeBackend/src/routes/auth.js
+++ NodeBackend/src/routes/auth.js
@@ -50,4 +50,76 @@ authRouter.post("/citizen-register", (req, res) => {
     }
 })
 
+authRouter.post("/login", (req, res) => {
+    try {
+        const { Email, Password } = req.body;
+
+        const statement = `select  UserId,FirstName,LastName,Email,Phone,Address,Pincode,State,District,City,RoleId,ActiveState from users where Email = ? and Password = ? `;
+        const hashedPassword = CryptoJS.SHA256(Password).toString(CryptoJS.enc.Base64);
+        db.pool.query(statement, [Email, hashedPassword], (err, users) => {
+            if (err) res.status(400).json({ message: err.message })
+
+            if (users.length == 0) res.status(404).json({ message: "Invalid Credentials" })
+            else {
+                const user = users[0];
+                if (user.ActiveState == 0) {
+                    res.status(410).json({ message: "User already deleted " });
+                } else {
+                    const payload = {
+                        UserId: user.UserId,
+                        Role: user.RoleId == 1 ? "Admin" : "Citizen",
+                        RoleId: user.RoleId
+                    }
+                    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
+                    res.cookie("token", token, {
+                        httpOnly: true,
+                        secure: false, // set true in production with HTTPS
+                        sameSite: "lax", // "none" if cross-origin & secure
+                        path: "/", // important to match logout clearCookie
+                        maxAge: 60 * 60 * 1000 // 1 hour in ms (optional)
+
+                    })
+                    res.json({
+                        user: user
+                    })
+
+                }
+
+
+            }   
+
+
+        })
+
+
+
+
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+
+    }
+})
+
+authRouter.post("/logout", (req, res) => {
+
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: false, // 🔐 Set to true in production (with HTTPS)
+            sameSite: "lax", // or "none" only if cross-site & over HTTPS
+            path: "/" // ✅ VERY important to match cookie path
+
+        })
+        console.log("cleared Cookies");
+        res.json({
+            ok: true,
+            message: "Logged Out"
+        })
+
+
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+
+    }
+})
 module.exports = authRouter;
